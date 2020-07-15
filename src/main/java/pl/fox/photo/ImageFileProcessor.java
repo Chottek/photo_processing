2280889fcac130305e08b4ab6799226955f89921
Chottek <bm.chotkowski@gmail.com>
2020-07-15 16:29:08 +0200

    Added some comments

--- src/main/java/pl/fox/photo/ImageFileProcessor.java
+++ src/main/java/pl/fox/photo/ImageFileProcessor.java
@@ -10,9 +10,9 @@ public class ImageFileProcessor implements IProcessor {
     private final ConfigHandler configHandler;
     private final Handler handler;
 
-    private ExecutorService pool; //for testing purposes only
+    private ExecutorService pool;
 
-    private int listSize;
+    private int listSize; //variable for getting a size of full images list
 
     public ImageFileProcessor() {
         handler = new Handler(this);
@@ -22,13 +22,13 @@ public class ImageFileProcessor implements IProcessor {
 
     @Override
     public void process(int borderValue) {
-        pool = Executors.newFixedThreadPool(configHandler.getMaxThreads());
-        listSize = imgReader.getImages().size();
-        for (File f : imgReader.getImages()) {
+        pool = Executors.newFixedThreadPool(configHandler.getMaxThreads()); //initializing thread pool with maximum size (given in config)
+        listSize = imgReader.getImages().size();  //getting full images list size
+        for (File f : imgReader.getImages()) {    //iterating over all images in list
             Runnable r = new ProcessorThread(f, borderValue, configHandler.getOutputFolder(), handler);
-            pool.execute(r);
+            pool.execute(r); //starting new thread to process image
         }
-        pool.shutdown();
+        pool.shutdown(); //shutting down the pool when finished
     }
 
     public ConfigHandler getConfigHandler() {
